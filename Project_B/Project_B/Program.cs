dcd9dfc02c8627ca17534e856277c255871e82da
Jacksn1219 <csvadeel@gmail.com>
2024-04-21 23:19:32 +0200

    Shit dit had er niet in moeten staan

--- Project_B/Project_B/Program.cs
+++ Project_B/Project_B/Program.cs
@@ -129,30 +129,6 @@ namespace Project_B
             {
                 //Aanmaken nieuwe film, acteur, regiseur, zaal.
             });
-            medewerkerMenu.Add("Send Mail", (x) =>
-            {
-                var email = new MimeMessage();
-
-                email.From.Add(new MailboxAddress("Sender Name", "sender@email.com"));
-                email.To.Add(new MailboxAddress("Receiver Name", "receiver@email.com"));
-
-                email.Subject = "Testing out email sending";
-                email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-                {
-                    Text = "<b>Hello all the way from the land of C#</b>"
-                };
-
-                using (var smtp = new SmtpClient())
-                {
-                    smtp.Connect("smtp.server.address", 587, false);
-
-                    // Note: only needed if the SMTP server requires authentication
-                    smtp.Authenticate("smtp_username", "smtp_password");
-
-                    smtp.Send(email);
-                    smtp.Disconnect(true);
-                }
-            });
 
             /*medewerkerMenu.Add("Setup Database", (x) =>
             {
